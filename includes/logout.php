b9458e3dcd6e735ec456b14f90b74d58bac69683
wuamchen47 <wuamchen47@me.com>
2015-01-29 14:46:28 +0100

    logout needs cfg

--- includes/logout.php
+++ includes/logout.php
@@ -1,4 +1,5 @@
 <?php
+include_once ($_SERVER['DOCUMENT_ROOT'] . "/../_globals/cfg_other.php");
 include_once ($_SERVER['DOCUMENT_ROOT'] . "/" . SITE . "/includes/functions.php");
 sec_session_start();
  
